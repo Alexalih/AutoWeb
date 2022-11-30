9909e9772d338522f24d01d01474e2b7ed80fa91
Alexlih <sasha-zba@mil.ru>
2022-11-30 20:58:43 +0300

    Homework4

--- src/main/java/Homework3/Test2LogOut.java
+++ src/main/java/Homework3/Test2LogOut.java
@@ -1,31 +1,34 @@
+package Homework3;
+
+import Homework3.Test1LogIn;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.concurrent.TimeUnit;
 
-public class Test4Search {
+public class Test2LogOut {
     public static void main(String[] args) {
+
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
         options.addArguments("start-maximized");
 
         WebDriver driver =new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.gloria-jeans.ru");
 
         Test1LogIn.CloseSelectRegion(driver);
+        Test1LogIn.logIn(driver);
 
-        WebElement searchField = driver.findElement(By.xpath(".//*[@id=\"js-site-search-input\"]"));
-        searchField.click();
-        searchField.sendKeys("пижамы");
-        WebElement lookMore = driver.findElement(By.xpath(".//button[@class=\"digi-ac-find__button\"]"));
-        lookMore.click();
+        Test1LogIn.CloseAlert(driver);
 
-        // driver.quit();
+
+        WebElement elementLog =driver.findElement(By.xpath("//div[@class=\"modal-account\"]/a[@href=\"/logout\"]"));
+        elementLog.click();
+
     }
 }
--- src/main/java/Homework3/Test3ChangeCity.java
+++ src/main/java/Homework3/Test3ChangeCity.java
@@ -1,3 +1,6 @@
+package Homework3;
+
+import Homework3.Test1LogIn;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
